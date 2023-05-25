0ad121d946f6effdb50a22cd80c90639f5b857b0
Fraser Molyneux <fmolyneux@microsoft.com>
2023-05-25 10:15:21 +0100

    Adding primary location to template

--- terraform/log-analytics-workspace.tf
+++ terraform/log-analytics-workspace.tf
@@ -1,8 +1,8 @@
 resource "azurerm_log_analytics_workspace" "law" {
-  name = format("la-%s-%s-%s", random_id.environment_id.hex, var.environment, var.locations[0])
+  name = format("la-%s-%s-%s", random_id.environment_id.hex, var.environment, var.primary_location)
 
-  location            = var.locations[0]
-  resource_group_name = azurerm_resource_group.rg[var.locations[0]].name
+  location            = var.primary_location
+  resource_group_name = azurerm_resource_group.rg[var.primary_location].name
 
   sku = "PerGB2018"
 
